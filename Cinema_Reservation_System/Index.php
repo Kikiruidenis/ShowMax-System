61cef0dc88eed70bea5032b8b82bbe2334114bba
Kikiruidenis <39088264+Kikiruidenis@users.noreply.github.com>
2022-06-09 14:04:18 +0300

    Add files via upload
    First commit

---
+++ Cinema_Reservation_System/Index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+   <title> Movie Reservation System| </title>
+    
+    <link rel="stylesheet" href="css/stylesheet.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <body>
+       <header>
+           <div>
+               <h1>Showmax Ticket Reservation System</h1>
+           <div class="log_out">
+          <a href="#">Log out </div></a>
+</div>
+<div>
+<li style="list-style-type:none">
+    <a href="#"><li>Home</li>
+  <a href="#"><li>Features</li>
+  <a href="#"><li>Demo</li>
+  <a href="#"><li>Fag</li>
+  <a href="#"><li>Pricing</li>
+  <a href="#"><li>Contat us</li>
+  </a>
+</li>  
+  </div>
+</header>
+
+
+  <div class="container">
+  <img src="img/showmax.jpg" alt="Notebook" style="width:100%;">
+  <div class="content">
+<h2>Welcme to ShowMax Cinema </h2>
+  <p>The home of variety of Entertaining movies and fun</p>
+</div>
+</div>
+  
+
+</body>
+</html> 
